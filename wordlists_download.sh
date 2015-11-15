5466cbb119b0687f4948760d6f2d92e3d8767a85
root <root@localhost.localdomain>
2015-11-15 19:56:30 +0000

    Wordlists Download Script

---
+++ wordlists_download.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+#Africaan
+wget http://www.md5this.com/wordlists/dictionary_africaan.zip
+#Australian
+wget http://www.md5this.com/wordlists/dictionary_australian.zip
+#Brazilian
+wget http://www.md5this.com/wordlists/dictionary_brazilian.zip
+#Chinese
+wget http://www.md5this.com/wordlists/dictionary_chinese.zip
+#Croatian
+wget http://www.md5this.com/wordlists/dictionary_croatian.zip
+#Czech
+wget http://www.md5this.com/wordlists/dictionary_czech.zip
+#Danish
+wget http://www.md5this.com/wordlists/dictionary_danish.zip
+#Dutch
+wget http://www.md5this.com/wordlists/dictionary_dutch.zip
+#English
+wget http://www.md5this.com/wordlists/dictionary_english.zip
+#Finnish
+wget http://www.md5this.com/wordlists/dictionary_finnish.zip
+#French
+wget http://www.md5this.com/wordlists/dictionary_french.zip
+#Fungi
+wget http://www.md5this.com/wordlists/dictionary_fungi.zip
+#German
+wget http://www.md5this.com/wordlists/dictionary_german.zip
+#Hindu
+wget http://www.md5this.com/wordlists/dictionary_hindu.zip
+#Hungarian
+wget http://www.md5this.com/wordlists/dictionary_hungarian.zip
+#Italian
+wget http://www.md5this.com/wordlists/dictionary_italian.zip
+#Japanese
+wget http://www.md5this.com/wordlists/dictionary_japanese.zip
+#Latin
+wget http://www.md5this.com/wordlists/dictionary_latin.zip
+#Norwegian
+wget http://www.md5this.com/wordlists/dictionary_norwegian.zip
+#Polish
+wget http://www.md5this.com/wordlists/dictionary_polish.zip
+#Portuguese
+wget http://www.md5this.com/wordlists/dictionary_portuguese.zip
+#Russian
+wget http://www.md5this.com/wordlists/dictionary_russian.zip
+#Spanish
+wget http://www.md5this.com/wordlists/dictionary_spanish.zip
+#Swahili
+wget http://www.md5this.com/wordlists/dictionary_swahili.zip
+#Swedish
+wget http://www.md5this.com/wordlists/dictionary_swedish.zip
+#Turkish
+wget http://www.md5this.com/wordlists/dictionary_turkish.zip
+#Yiddish
+wget http://www.md5this.com/wordlists/dictionary_yiddish.zip
+
+#Two Words
+wget http://www.md5this.com/wordlists/dictionary_two_words.zip
+#Thematic
+wget http://www.md5this.com/wordlists/dictionary_misc.zip
+#Thematic (Sorted)
+wget http://www.md5this.com/wordlists/dictionary_misc_sorted.zip
+#Numbers (0...9999999)
+wget http://www.md5this.com/wordlists/dictionary_numbers_1-7.zip
+
+#Huge English (A)
+wget http://www.md5this.com/wordlists/dictionary_huge_a.zip
+#Huge English (B)
+wget http://www.md5this.com/wordlists/dictionary_huge_b.zip
+#Huge English (C)
+wget http://www.md5this.com/wordlists/dictionary_huge_c.zip
+#Huge English (D)
+wget http://www.md5this.com/wordlists/dictionary_huge_d.zip
+#Huge English (E)
+wget http://www.md5this.com/wordlists/dictionary_huge_e.zip
+#Huge English (F)
+wget http://www.md5this.com/wordlists/dictionary_huge_f.zip
+#Huge English (G)
+wget http://www.md5this.com/wordlists/dictionary_huge_g.zip
+#Huge English (H)
+wget http://www.md5this.com/wordlists/dictionary_huge_h.zip
+#Huge English (I)
+wget http://www.md5this.com/wordlists/dictionary_huge_i.zip
+#Huge English (J)
+wget http://www.md5this.com/wordlists/dictionary_huge_j.zip
+#Huge English (K)
+wget http://www.md5this.com/wordlists/dictionary_huge_k.zip
+#Huge English (L)
+wget http://www.md5this.com/wordlists/dictionary_huge_l.zip
+#Huge English (M)
+wget http://www.md5this.com/wordlists/dictionary_huge_m.zip
+#Huge English (N)
+wget http://www.md5this.com/wordlists/dictionary_huge_n.zip
+#Huge English (O)
+wget http://www.md5this.com/wordlists/dictionary_huge_o.zip
+#Huge English (P)
+wget http://www.md5this.com/wordlists/dictionary_huge_p.zip
+#Huge English (Q)
+wget http://www.md5this.com/wordlists/dictionary_huge_q.zip
+#Huge English (R)
+wget http://www.md5this.com/wordlists/dictionary_huge_r.zip
+#Huge English (S)
+wget http://www.md5this.com/wordlists/dictionary_huge_s.zip
+#Huge English (T)
+wget http://www.md5this.com/wordlists/dictionary_huge_t.zip
+#Huge English (U)
+wget http://www.md5this.com/wordlists/dictionary_huge_u.zip
+#Huge English (V)
+wget http://www.md5this.com/wordlists/dictionary_huge_v.zip
+#Huge English (W)
+wget http://www.md5this.com/wordlists/dictionary_huge_w.zip
+#Huge English (X)
+wget http://www.md5this.com/wordlists/dictionary_huge_x.zip
+#Huge English (Y)
+wget http://www.md5this.com/wordlists/dictionary_huge_y.zip
+#Huge English (Z)
+wget http://www.md5this.com/wordlists/dictionary_huge_z.zip
+
+#Huge English (0)
+wget http://www.md5this.com/wordlists/dictionary_huge_0.zip
+#Huge English (1)
+wget http://www.md5this.com/wordlists/dictionary_huge_1.zip
+#Huge English (2)
+wget http://www.md5this.com/wordlists/dictionary_huge_2.zip
+#Huge English (3)
+wget http://www.md5this.com/wordlists/dictionary_huge_3.zip
+#Huge English (4)
+wget http://www.md5this.com/wordlists/dictionary_huge_4.zip
+#Huge English (5)
+wget http://www.md5this.com/wordlists/dictionary_huge_5.zip
+#Huge English (6)
+wget http://www.md5this.com/wordlists/dictionary_huge_6.zip
+#Huge English (7)
+wget http://www.md5this.com/wordlists/dictionary_huge_7.zip
+#Huge English (8)
+wget http://www.md5this.com/wordlists/dictionary_huge_8.zip
+#Huge English (9)
+wget http://www.md5this.com/wordlists/dictionary_huge_9.zip
+#Huge English (!@#...)
+wget http://www.md5this.com/wordlists/dictionary_huge_spec.zip
+
+#CrackStation (.torrent file)(crackstation.txt.gz)
+wget https://crackstation.net/downloads/crackstation.txt.gz.torrent
+#CrackStation Human Passwords Only (.torrent file)(crackstation-human-only.txt.gz)
+wget https://crackstation.net/downloads/crackstation-human-only.txt.gz.torrent
+
